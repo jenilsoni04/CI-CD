3c2db80ae7226066f4d6164c8cb9772c071a4632
Jenil Vipulsoni <jenilvipulsoni22@gmail.com>
2025-11-14 01:37:40 +0530

    inital commit

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+// Name : Jenil Soni
+// Student Id : 202412108
+
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+
+const register = client.register;
+client.collectDefaultMetrics();
+
+const requestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests'
+});
+
+app.use((req, res, next) => {
+  requestCounter.inc();
+  next();
+});
+
+// Status endpoint
+app.get('/status', (req, res) => {
+  res.json({
+    status: 'ok',
+    time: new Date().toISOString()
+  });
+});
+
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+  try {
+    const metrics = await register.metrics();
+    res.set('Content-Type', register.contentType);
+    res.send(metrics);
+  } catch (err) {
+    res.status(500).send(err.toString());
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
